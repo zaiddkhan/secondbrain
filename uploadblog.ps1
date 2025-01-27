ebab3d4fbdad72199851a85592194f7102c395fc
zaiddkhan <zaiddkhhan@gmail.com>
2025-01-27 15:11:10 +0530

    New Blog Post on 2025-01-27 15:11:10

--- uploadblog.ps1
+++ uploadblog.ps1
@@ -119,39 +119,12 @@ if (-not $hasStagedChanges) {
 # Step 7: Push all changes to the main branch
 Write-Host "Deploying to GitHub Master..."
 try {
-    git push origin master
+    git push origin main
 } catch {
     Write-Error "Failed to push to Master branch."
     exit 1
 }
 
-# Step 8: Push the public folder to the hostinger branch using subtree split and force push
-Write-Host "Deploying to GitHub Hostinger..."
 
-# Check if the temporary branch exists and delete it
-$branchExists = git branch --list "hostinger-deploy"
-if ($branchExists) {
-    git branch -D hostinger-deploy
-}
-
-# Perform subtree split
-try {
-    git subtree split --prefix public -b hostinger-deploy
-} catch {
-    Write-Error "Subtree split failed."
-    exit 1
-}
-
-# Push to hostinger branch with force
-try {
-    git push origin hostinger-deploy:hostinger --force
-} catch {
-    Write-Error "Failed to push to hostinger branch."
-    git branch -D hostinger-deploy
-    exit 1
-}
-
-# Delete the temporary branch
-git branch -D hostinger-deploy
 
 Write-Host "All done! Site synced, processed, committed, built, and deployed."
